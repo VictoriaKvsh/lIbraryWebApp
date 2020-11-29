4a20ab1a2366990bea8e66cd149c81c9127f5f3f
DESKTOP-B1B7OFG\v1k4a <kuvshinova@gmail.com>
2020-11-29 14:16:34 +0300

    no message

--- librarywebapp/src/main/java/by/grodno/vika/librarywebapp/Starter.java
+++ librarywebapp/src/main/java/by/grodno/vika/librarywebapp/Starter.java
@@ -1,4 +1,4 @@
-package librarywebapp;
+package by.grodno.vika.librarywebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
